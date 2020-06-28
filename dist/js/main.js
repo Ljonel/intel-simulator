ec5a129e437c7ab561e0a0e4072385bf1e0b521c
Damian Cygan <damian.cygan@microsoft.wsei.edu.pl>
2020-06-28 18:06:52 +0200

    MOV

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,48 +1,33 @@
-var item;
-document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById("btn").addEventListener('click', function(){
-       
-        let ax = document.getElementsByName("item")[0].value; //input ax
-        let bx = document.getElementsByName("item")[1].value; //input bx
-        let cx = document.getElementsByName("item")[2].value;  
-        let dx = document.getElementsByName("item")[3].value;
-        let tab = [ax,bx,cx,dx];
-        let tabParse = [];
-
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("btn").addEventListener('click', () => {
+        const ax = document.getElementsByName("item")[0];
+        const bx = document.getElementsByName("item")[1];
+        const cx = document.getElementsByName("item")[2];
+        const dx = document.getElementsByName("item")[3];
+ 
+        const paramsMap = {
+            AX: ax,
+            BX: bx,
+            CX: cx,
+            DX: dx,
+        };
+ 
         const regex = RegExp('^[0-1]{1,}$');
-        console.log(regex.test(ax));
-        
-        if(ax == '' || bx==''||cx==''||dx==''){
+ 
+        if (ax.value === '' || bx.value === '' || cx.value === '' || dx.value === '') {
             alert("You have to enter all data");
+        } else if (regex.test(ax.value) && regex.test(bx.value) && regex.test(cx.value) && regex.test(dx.value)) {
+            const cmd = document.getElementById("command").value;
+            const firstParam = document.getElementById("par1").value;
+            const secondParam = document.getElementById("par2").value;
+ 
+            if (cmd === "MOV") {
+                paramsMap[firstParam].value = paramsMap[secondParam].value;
+                paramsMap[secondParam].value = paramsMap[firstParam].value;
+            }
         }
-        else if(regex.test(ax) && regex.test(bx) && regex.test(cx) && regex.test(dx)){
-            
-            for(let i =0;i<tab.length;i++){
-                tabParse[i] = parseInt(tab[i],2);
-             }
-            
-             let cmd = document.getElementById("command").value; //MOV ADD XUB
-             let parametr1 = document.getElementById("par1").value; // PARAMETR1
-             let parametr2 = document.getElementById("par2").value; //PARAMETR2
-
-             if(cmd == "MOV")                       
-             {
-                if(parametr1 == "AX" && parametr2 == "BX") // NO I TU BY MUSIALO BYC MILION WARUNKÓW
-                let temp = tabParse[0];
-                 tabParse[0] = tabParse[1];
-                 tabParse[1] = temp;
-                 document.getElementsByName("item")[0].value = tabParse[0].toString(2);
-                 document.getElementsByName("item")[1].value = tabParse[1].toString(2);
-             }
-        }
-        else{
+        else {
             alert("Use binary code!");
         }
-        
-      function Move(cmd){
-        
-      }
-      
     });
 });
-
